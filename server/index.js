385a185d6eebb829daa776ed0a4683ce50d9c048
Kira Ablamunits <kira.abl@gmail.com>
2022-01-15 20:37:48 +0200

    change client/ build to build

--- server/index.js
+++ server/index.js
@@ -282,11 +282,11 @@ app.get('/download', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "index.html"));
 });
 
 
