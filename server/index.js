6467191d072203a90006cbcf8b7dec10b3b297a3
Kira Ablamunits <kira.abl@gmail.com>
2022-01-19 14:27:40 +0200

    change the start again button

--- server/index.js
+++ server/index.js
@@ -286,7 +286,7 @@ app.use(express.static(path.join(__dirname, "build")));
 
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
